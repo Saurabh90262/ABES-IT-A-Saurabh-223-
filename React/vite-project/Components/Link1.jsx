44700d4077c0d547a9d18ca3b81247bec713e080
Saurabh Kumar <saurabh90262@gmail.com>
2025-11-10 13:01:23 +0530

    updated Book App

---
+++ React/vite-project/Components/Link1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function Link1() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Link1
--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,18 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Usestate from "../Components/Usestate";
-import Count1 from "../Components/Count1";
+import React from 'react'
+import Link1 from '../Components/Link1'
+import { Route, Routes } from 'react-router-dom'
+import{Routes,Route,Link} from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="container">
-      <Count1 />
-    </div>
-  );
+function Home() {
+  return <h1>Welcome to the Home Page</h1>
 }
 
-export default App;
+function About() {
+  return <h1>About Us Page</h1>
+}
+function Link1() {
+  return (
+    <a href={to} style={{ margin: '0 10px' }}>
+      {children}
+    </a>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <Link1 to="/">Home</Link1>
+        <Link1 to="/about">About us</Link1>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About us />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
